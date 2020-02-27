62d2b74cdf374dc84ed5b2eb47fa123446740369
Daniel Alejandro Angel Fuertes <da.angelf@uniandes.edu.co>
2020-02-26 23:46:48 -0500

    tests ordenamiento

--- test/test/data_structures/TestOrdenamiento.java
+++ test/test/data_structures/TestOrdenamiento.java
@@ -1,50 +1,177 @@
 package test.data_structures;
 import model.data_structures.ArregloDinamico;
+import model.data_structures.Ordenamientos;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestOrdenamiento {
+public class TestOrdenamiento 
+{
 
 	
 
 		private ArregloDinamico<Integer> arreglo;
 		private static int TAMANO=100;
+		private Ordenamientos ordenar;
 
 		@Before
-		public void setUp1() {
-			arreglo= new ArregloDinamico(TAMANO);
-		}
+		public void setUp1() 
+		{
+			arreglo= new ArregloDinamico<Integer>(10);
+			arreglo.agregar(1);
+			arreglo.agregar(6);
+			arreglo.agregar(7);
+			arreglo.agregar(2);
+			arreglo.agregar(8);
+			arreglo.agregar(4);
+			arreglo.agregar(0);
+			arreglo.agregar(3);
+			arreglo.agregar(9);
+			arreglo.agregar(5);
 
+			
+
+
+
+		}
+		@Before
 		public void setUp2() {
-			for(int i =0; i< TAMANO*2; i++){
-				arreglo.agregar(i);
-			}
-		}
+			arreglo= new ArregloDinamico<Integer>(10);
+			arreglo.agregar(0);
+			arreglo.agregar(1);
+			arreglo.agregar(2);
+			arreglo.agregar(3);
+			arreglo.agregar(4);
+			arreglo.agregar(5);
+			arreglo.agregar(6);
+			arreglo.agregar(7);
+			arreglo.agregar(8);
+			arreglo.agregar(9);
 
+		}
+		@Before
+		public void setU3() {
+			arreglo= new ArregloDinamico<Integer>(10);
+			arreglo.agregar(9);
+			arreglo.agregar(8);
+			arreglo.agregar(7);
+			arreglo.agregar(6);
+			arreglo.agregar(5);
+			arreglo.agregar(4);
+			arreglo.agregar(3);
+			arreglo.agregar(2);
+			arreglo.agregar(1);
+			arreglo.agregar(0);
+
+		}
 		@Test
 		public void testArregloDinamico() {
 
 			setUp2();
-			assertEquals("El arreglo no tiene el tamaño espera", 200  ,arreglo.darTamano());
+			assertEquals("El arreglo no tiene el tamaño espera", 10  ,arreglo.darTamano());
 			arreglo.eliminar(0);
-			assertEquals("El arreglo no tiene el tamaño espera",199, arreglo.darTamano());
+			assertEquals("El arreglo no tiene el tamaño espera",9, arreglo.darTamano());
 			// TODO
 		}
 
 		@Test
 		public void testDarElemento() 
 		{
+			setUp1();
+			// TODO
+			assertEquals("No es el elemento esperado",6 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",4 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(9));
 			setUp2();
 			// TODO
-			assertEquals("No es el elemento esperado",1 , arreglo.darElemento(1));
-			assertEquals("No es el elemento esperado",50 , arreglo.darElemento(50));
-			assertEquals("No es el elemento esperado",199 , arreglo.darElemento(199));
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setU3();
+			// TODO
+			assertEquals("No es el elemento esperado",8 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",4 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",0 , (int)arreglo.darElemento(9));
+		}
+		@Test
+		public void testShellSort() 
+		{
+			setUp1();
+			// TODO
+			Ordenamientos.ShellSort(arreglo.darElementos());
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setUp2();
+			// TODO
+			Ordenamientos.ShellSort(arreglo.darElementos());
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setU3();
+			// TODO
+			Ordenamientos.ShellSort(arreglo.darElementos());
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			
+		}
+		@Test
+		public void testMergeSort() 
+		{
+			setUp1();
+			// TODO
+			Ordenamientos.sortMerge(arreglo.darElementos(), 0,arreglo.darTamano()-1);
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setUp2();
+			// TODO
+			Ordenamientos.sortMerge(arreglo.darElementos(), 0,arreglo.darTamano()-1);
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setU3();
+			// TODO
+			Ordenamientos.sortMerge(arreglo.darElementos(), 0,arreglo.darTamano()-1);
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+		}
+		@Test
+		public void testDarQuickSort() 
+		{
+			setUp1();
+			// TODO
+			Ordenamientos.Quicksort(arreglo.darElementos());
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setUp2();
+			// TODO
+			Ordenamientos.Quicksort(arreglo.darElementos());
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
+			setU3();
+			// TODO
+			Ordenamientos.Quicksort(arreglo.darElementos());
+
+			assertEquals("No es el elemento esperado",1 , (int)arreglo.darElemento(1));
+			assertEquals("No es el elemento esperado",5 , (int)arreglo.darElemento(5));
+			assertEquals("No es el elemento esperado",9 , (int)arreglo.darElemento(9));
 		}
 
 	}
 
 
-}
+
